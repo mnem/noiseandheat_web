e24cca5057db598d0fc398d4b4faf849da30e530
David Wagner <mnem@noiseandheat.com>
2010-10-05 20:35:47 +0100

    Fixing url message query

--- subdomains/wip/content/binday/js/binday.js
+++ subdomains/wip/content/binday/js/binday.js
@@ -6,7 +6,9 @@ var BIN_DELIM = " ";
 
 function getUrlVars() {
 	var vars = [], hash;
-	var hashes = decodeURI(window.location.href.slice(window.location.href.indexOf('?') + 1)).split('&');
+	var query = decodeURI(window.location.href.slice(window.location.href.indexOf('?') + 1));
+	console.log("query: " + query);
+	var hashes = query.split('&');
  
 	for(var i = 0; i < hashes.length; i++) {
 		hash = hashes[i].split('=');
