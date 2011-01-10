7f553b486a7a64de2927cac80ba63a167863bd0c
David Wagner <mnem@noiseandheat.com>
2011-01-10 01:53:50 +0000

    Added some comments to the js source

--- subdomains/wip/content/andrew_scrolly/js/init.js
+++ subdomains/wip/content/andrew_scrolly/js/init.js
@@ -1,9 +1,14 @@
+// This function handles the razor link being clicked
+// It's attached to the link in the document ready
+// function at the end of the source file
 function go_razor() {
+	// Scrolls to the element with the ID "razor"
+	// over 1 second (1000 milliseconds)
 	$.scrollTo("#razor", 1000);
 }
 
+// This code runs after the web page has loaded
 $(document).ready(function() {
-	$('div.pane').scrollTo( 0 );
-	$.scrollTo( 0 );
+	// Find the go-razor link and add a click event handler
 	$("#go-razor").click(go_razor);
 });
